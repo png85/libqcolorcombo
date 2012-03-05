e02bbe42940bd2a0d4d6739f392d21c03255f9cd
Peter Hille (png!das-system) <peter@das-system-networks.de>
2012-03-05 21:16:32 +0100

    Add .ts file for translations.

---
+++ qcolorcombo.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>QColorCombo</name>
+    <message>
+        <location filename="qcolorcombo.cc" line="22"/>
+        <location filename="qcolorcombo.cc" line="38"/>
+        <source>Fatal error!</source>
+        <translation>Schwerwiegender Fehler!</translation>
+    </message>
+    <message>
+        <location filename="qcolorcombo.cc" line="23"/>
+        <source>Unable to allocate memory for new QHBoxLayout!</source>
+        <translation>Konnte speicher für QHBoxLayout nicht reservieren!</translation>
+    </message>
+    <message>
+        <location filename="qcolorcombo.cc" line="39"/>
+        <source>Unable to allocate memory for new QComboBox!</source>
+        <translation>Konnte Speicher für QComboBox nicht reservieren!</translation>
+    </message>
+    <message>
+        <location filename="qcolorcombo.cc" line="44"/>
+        <source>Custom color...</source>
+        <translation>Benutzerdefiniert...</translation>
+    </message>
+</context>
+</TS>
