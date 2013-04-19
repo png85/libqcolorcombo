e374679652319c317ea139289e11e7a344becb0e
Peter Hille (png!das-system) <peter@das-system-networks.de>
2013-04-19 23:47:38 +0200

    add translation files for en_US/de_DE locales

---
+++ QColorCombo-de_DE.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>QColorCombo</name>
+    <message>
+        <location filename="QColorCombo.cc" line="49"/>
+        <source>Caught exception when trying to allocate new QHBoxLayout in %1: %2</source>
+        <translation>Exception vom Typ &quot;%2&quot; beim Anlegen von neuer QHBoxLayout-Instanz in %1</translation>
+    </message>
+    <message>
+        <location filename="QColorCombo.cc" line="50"/>
+        <location filename="QColorCombo.cc" line="61"/>
+        <source>Out of memory</source>
+        <translation>Zu wenig Arbeitsspeicher</translation>
+    </message>
+    <message>
+        <location filename="QColorCombo.cc" line="60"/>
+        <source>Caught exception when trying to allocate new QComboBox in %1: %2</source>
+        <translation>Exception vom Typ &quot;%2&quot; beim Anlegen von neuer QComboBox-Instanz in %1 </translation>
+    </message>
+    <message>
+        <location filename="QColorCombo.cc" line="152"/>
+        <source>Custom...</source>
+        <translation>Andere...</translation>
+    </message>
+</context>
+</TS>
---
+++ QColorCombo-en_US.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>QColorCombo</name>
+    <message>
+        <location filename="QColorCombo.cc" line="49"/>
+        <source>Caught exception when trying to allocate new QHBoxLayout in %1: %2</source>
+        <translation>Caught exception of type &quot;%2&quot; when trying to allocate new QHBoxLayout in %1</translation>
+    </message>
+    <message>
+        <location filename="QColorCombo.cc" line="50"/>
+        <location filename="QColorCombo.cc" line="61"/>
+        <source>Out of memory</source>
+        <translation>Out of memory</translation>
+    </message>
+    <message>
+        <location filename="QColorCombo.cc" line="60"/>
+        <source>Caught exception when trying to allocate new QComboBox in %1: %2</source>
+        <translation>Caught exception of type &quot;%2&quot; when trying to allocate new QComboBox in %1</translation>
+    </message>
+    <message>
+        <location filename="QColorCombo.cc" line="152"/>
+        <source>Custom...</source>
+        <translation>Custom...</translation>
+    </message>
+</context>
+</TS>
