bde391fbbffd1975df14527b90441d16e3b2c9df
Peter Hille (png!das-system) <peter@das-system-networks.de>
2013-04-20 00:04:15 +0200

    add translation files for Qt Designer plugin

---
+++ QColorComboPlugin-de_DE.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>QColorComboDesignerFactory</name>
+    <message>
+        <location filename="QColorComboDesignerFactory.cc" line="25"/>
+        <source>Caught exception when trying to create new QBrowseButtoninstance in %1: %2</source>
+        <translation>Ausnahme vom Typ &quot;%2&quot; beim Erstellen von neuer QBrowseButton-Instanz in %1</translation>
+    </message>
+    <message>
+        <location filename="QColorComboDesignerFactory.cc" line="27"/>
+        <source>Out of memory</source>
+        <translation>Zu wenig Arbeitsspeicher</translation>
+    </message>
+    <message>
+        <location filename="QColorComboDesignerFactory.cc" line="50"/>
+        <source>Color combo box</source>
+        <translation>Combo-Box zur Farbauswahl</translation>
+    </message>
+    <message>
+        <location filename="QColorComboDesignerFactory.cc" line="55"/>
+        <source>QComboBox that can be used to select QColor values</source>
+        <translation>QComboBox zur Farbauswahl</translation>
+    </message>
+</context>
+</TS>
---
+++ QColorComboPlugin-en_US.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>QColorComboDesignerFactory</name>
+    <message>
+        <location filename="QColorComboDesignerFactory.cc" line="25"/>
+        <source>Caught exception when trying to create new QBrowseButtoninstance in %1: %2</source>
+        <translation>Caught exception of type &quot;%2&quot; when trying to create new ColorCombo instance in %1</translation>
+    </message>
+    <message>
+        <location filename="QColorComboDesignerFactory.cc" line="27"/>
+        <source>Out of memory</source>
+        <translation>Out of memory</translation>
+    </message>
+    <message>
+        <location filename="QColorComboDesignerFactory.cc" line="50"/>
+        <source>Color combo box</source>
+        <translation>Color combo box</translation>
+    </message>
+    <message>
+        <location filename="QColorComboDesignerFactory.cc" line="55"/>
+        <source>QComboBox that can be used to select QColor values</source>
+        <translation>QComboBox that can be used to select QColor values</translation>
+    </message>
+</context>
+</TS>
