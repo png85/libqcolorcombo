9ebf9eace091633e1ebfcaf4d2dbb36b313e391d
Peter Hille (png!das-system) <peter@das-system-networks.de>
2013-04-19 23:23:03 +0200

    create translation file for Qt Designer plugin

---
+++ QColorComboPlugin.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>QColorComboDesignerFactory</name>
+    <message>
+        <location filename="QColorComboDesignerFactory.cc" line="25"/>
+        <source>Caught exception when trying to create new QBrowseButtoninstance in %1: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QColorComboDesignerFactory.cc" line="27"/>
+        <source>Out of memory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QColorComboDesignerFactory.cc" line="50"/>
+        <source>Color combo box</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QColorComboDesignerFactory.cc" line="55"/>
+        <source>QComboBox that can be used to select QColor values</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
