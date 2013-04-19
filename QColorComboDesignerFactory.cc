b848a16ddb18e0980856f97760cc9c30cd5d11de
Peter Hille (png!das-system) <peter@das-system-networks.de>
2013-04-19 19:53:00 +0200

    add optional Qt Designer plugin for QColorCombo

---
+++ QColorComboDesignerFactory.cc
@@ -0,0 +1,67 @@
+#include <QDebug>
+#include <QMessageBox>
+#include <QtPlugin>
+
+#include "nullptr.h"
+#include "QColorCombo.h"
+#include "QColorComboDesignerFactory.h"
+
+
+QColorComboDesignerFactory::QColorComboDesignerFactory(QDeclarativeItem *parent) :
+    QObject(parent)
+{
+    // ... nothing ...
+}
+
+
+QWidget* QColorComboDesignerFactory::createWidget(QWidget *parent) {
+    QColorCombo* widget = nullptr;
+    try {
+        widget = new QColorCombo(parent);
+    }
+
+    catch (std::bad_alloc& ex) {
+        qCritical() << "Caught exception when trying to allocat new QColorCombo in" << Q_FUNC_INFO << ":" << ex.what();
+        QString message = tr("Caught exception when trying to create new QBrowseButton"
+                             "instance in %1: %2").arg(Q_FUNC_INFO, ex.what());
+        QMessageBox::critical(parent, tr("Out of memory"), message, QMessageBox::Ok);
+        throw;
+    }
+
+    return reinterpret_cast<QWidget*>(widget);
+}
+
+QString QColorComboDesignerFactory::group() const {
+    return "#das-system";
+}
+
+
+QIcon QColorComboDesignerFactory::icon() const {
+    return QIcon::fromTheme("video-display");
+}
+
+
+QString QColorComboDesignerFactory::includeFile() const {
+    return "QColorCombo.h";
+}
+
+
+QString QColorComboDesignerFactory::toolTip() const {
+    return tr("Color combo box");
+}
+
+
+QString QColorComboDesignerFactory::whatsThis() const {
+    return tr("QComboBox that can be used to select QColor values");
+}
+
+bool QColorComboDesignerFactory::isContainer() const {
+    return false;
+}
+
+
+QString QColorComboDesignerFactory::name() const {
+    return "QColorCombo";
+}
+
+Q_EXPORT_PLUGIN2(QColorComboDesignerPlugin, QColorComboDesignerFactory)
---
+++ QColorComboDesignerFactory.h
@@ -0,0 +1,31 @@
+#ifndef QCOLORCOMBODESIGNERFACTORY_H
+#define QCOLORCOMBODESIGNERFACTORY_H
+
+#include <QObject>
+#include <QtDeclarative/QDeclarativeItem>
+#include <QtDesigner/QDesignerCustomWidgetInterface>
+
+class QColorComboDesignerFactory : public QObject,
+        public QDesignerCustomWidgetInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(QDesignerCustomWidgetInterface)
+public:
+    explicit QColorComboDesignerFactory(QDeclarativeItem *parent = 0);
+    
+    virtual QWidget* createWidget(QWidget *parent);
+    virtual QString group() const;
+    virtual QIcon icon() const;
+    virtual QString includeFile() const;
+    virtual bool isContainer() const;
+    virtual QString name() const;
+    virtual QString toolTip() const;
+    virtual QString whatsThis() const;
+
+signals:
+    
+public slots:
+    
+};
+
+#endif // QCOLORCOMBODESIGNERFACTORY_H
