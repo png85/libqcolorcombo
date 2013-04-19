c27b1abb0d9b0d3c263285f553b939e66de12352
Peter Hille (png!das-system) <peter@das-system-networks.de>
2013-04-19 18:45:00 +0200

    fix translation file generation in CMake

---
+++ QColorCombo.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>QColorCombo</name>
+    <message>
+        <location filename="QColorCombo.cc" line="49"/>
+        <source>Caught exception when trying to allocate new QHBoxLayout in %1: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QColorCombo.cc" line="50"/>
+        <location filename="QColorCombo.cc" line="61"/>
+        <source>Out of memory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QColorCombo.cc" line="60"/>
+        <source>Caught exception when trying to allocate new QComboBox in %1: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QColorCombo.cc" line="152"/>
+        <source>Custom...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
