01a77b2da0fac3ad18e4eddac24967fa4da500d8
Peter Hille (png!das-system) <peter@das-system-networks.de>
2015-05-03 20:57:13 +0200

    Fix install path for Designer plugin

--- QColorComboPlugin.ts
+++ QColorComboPlugin.ts
@@ -6,7 +6,6 @@
     <message>
         <location filename="QColorComboDesignerFactory.cc" line="25"/>
         <source>Caught exception when trying to create new QColorComboinstance in %1: %2</source>
-        <oldsource>Caught exception when trying to create new QBrowseButtoninstance in %1: %2</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
